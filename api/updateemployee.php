e933c1ecc027d3163d7ca38e3fb2c943ee3ab2c1
mahdiauthlab <mahdi.authlab@gmail.com>
2022-04-29 02:30:29 +0600

    Update updateemployee.php

--- api/updateemployee.php
+++ api/updateemployee.php
@@ -1,3 +1,35 @@
 <?php
 
-?>
+    include_once "../controller/database.php";
+    include_once "../encoder/json_encoder.php";
+
+    if($_SERVER['REQUEST_METHOD'] != 'PUT'){
+
+        json_encoder("400 Error", 
+        ['error'=> "The request method isn't a valid request method, consider making a UPDATE request"]);
+    }else{
+        $connect = new Database();
+
+        $data_in_json = file_get_contents('php://input');
+    
+        $data = json_decode($data_in_json, true);
+
+        // foreach($data as $key => $value){
+            echo "$key - $value";
+        }
+
+        var_dump($data);
+
+    //     if(isset($data['id'])){
+    //         $id = $data['id'];
+
+    // $connect->deleteEmployee($id);
+    //     }else{
+    //         json_encoder("400 Error", 
+    //         ['error'=> "Please provide an id to delete data"]);
+    //     }
+    }
+
+   
+
+    
